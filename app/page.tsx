5b6aa26cbbd06322a53e1ad9901327c93f926f43
Chromax-design <chromax.design@gmail.com>
2025-05-24 13:53:00 +0100

    added framer scroll progress animation

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,33 @@ import Preloader from "@/components/Preloader";
 import Speakers from "@/components/Speakers";
 import { useEffect, useState } from "react";
 
+import { motion, useScroll } from "framer-motion";
+
 const Home = () => {
+  const { scrollYProgress } = useScroll();
+
+  return (
+    <>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 4,
+          originX: 0,
+          backgroundColor: "#ff0088",
+          zIndex: 50,
+        }}
+      />
+      <HomeContent />
+    </>
+  );
+};
+
+const HomeContent = () => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
